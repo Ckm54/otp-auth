873c0c8bdccc9680fda34729d2eb1da4e1187efc
collo <mutaic84@gmail.com>
2023-01-01 14:00:04 +0300

    :technologist: upate code structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_auth_firebase/provider/auth_provider.dart';
 import 'package:phone_auth_firebase/screens/welcome_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,10 +15,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
-      title: "Flutter Phone Auth",
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider())
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: WelcomeScreen(),
+        title: "Flutter Phone Auth",
+      ),
     );
   }
 }

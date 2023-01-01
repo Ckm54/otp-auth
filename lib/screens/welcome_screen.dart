fa16d5a944e43cbcd2c20253ec291ebf78c25ff8
collo <mutaic84@gmail.com>
2023-01-01 09:38:06 +0300

    :hammer: :lipstick: update welcome screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:phone_auth_firebase/widgets/custom_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -11,7 +12,37 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/image1.png", height: 300),
+                const SizedBox(height: 20),
+                const Text("Let's get started",
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 10),
+                const Text("Never a better time to start than now.",
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black38,
+                        fontWeight: FontWeight.bold)),
+                const SizedBox(height: 20),
+                // custom button
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: CustomButton(text: "Get Started", onPressed: () {}),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

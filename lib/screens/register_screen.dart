8dc997e137bde3dcc9e31f3209b2602575c83315
collo <mutaic84@gmail.com>
2023-01-01 11:55:05 +0300

    :hammer: :lipstick: add register page

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,146 @@
+import 'package:country_picker/country_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:phone_auth_firebase/widgets/custom_button.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController phoneController = TextEditingController();
+  Country selectedCountry = Country(
+      phoneCode: "254",
+      countryCode: "KE",
+      e164Sc: 0,
+      geographic: true,
+      level: 1,
+      name: "Kenya",
+      example: "Kenya",
+      displayName: "Kenya",
+      displayNameNoCountryCode: "KE",
+      e164Key: "");
+
+  @override
+  Widget build(BuildContext context) {
+    phoneController.selection = TextSelection.fromPosition(
+      TextPosition(
+        offset: phoneController.text.length,
+      ),
+    );
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: 200,
+                  height: 200,
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.purple.shade50),
+                  child: Image.asset("assets/image2.png"),
+                ),
+                const SizedBox(height: 20.0),
+                const Text("Register",
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 10),
+                const Text(
+                  "Enter phone number to receive verification code.",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  cursorColor: Colors.purple,
+                  controller: phoneController,
+                  onChanged: (value) {
+                    setState(() {
+                      phoneController.text = value;
+                    });
+                  },
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                  decoration: InputDecoration(
+                      hintText: "Enter phone number",
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Colors.grey.shade600,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.black12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.black12),
+                      ),
+                      prefix: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          onTap: () {
+                            showCountryPicker(
+                                context: context,
+                                countryListTheme: const CountryListThemeData(
+                                  bottomSheetHeight: 550,
+                                ),
+                                onSelect: (value) {
+                                  setState(() {
+                                    selectedCountry = value;
+                                  });
+                                });
+                          },
+                          child: Text(
+                            "${selectedCountry.flagEmoji} +${selectedCountry.phoneCode}",
+                            style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      suffix: phoneController.text.length > 8
+                          ? Container(
+                              height: 30,
+                              width: 30,
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 10.0),
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.green,
+                              ),
+                              child: const Icon(
+                                Icons.done,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                            )
+                          : null),
+                ),
+                const SizedBox(height: 20.0),
+
+                // login button
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: CustomButton(text: "Login", onPressed: () {}),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

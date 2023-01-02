fb62364648733d70df629c79eec6dd7e18079840
collo <mutaic84@gmail.com>
2023-01-02 04:41:51 +0300

    :hammer: :lipstick: ui update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:phone_auth_firebase/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,9 +12,35 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text("Flutter Phone Auth"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
       body: Center(
-        child: Text("Home Screen"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.purple,
+              backgroundImage: NetworkImage(authProvider.usersModel.profilePic),
+              radius: 50,
+            ),
+            const SizedBox(height: 20),
+            Text(authProvider.usersModel.name),
+            Text(authProvider.usersModel.phoneNumber),
+            Text(authProvider.usersModel.email),
+            Text(authProvider.usersModel.bio),
+          ],
+        ),
       ),
     );
   }

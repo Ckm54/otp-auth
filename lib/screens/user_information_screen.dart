a892fa45444cab33dce6e43fb86b7594294c2889
collo <mutaic84@gmail.com>
2023-01-02 04:19:36 +0300

    :lipstick: :hammer: widget update

--- lib/screens/user_information_screen.dart
+++ lib/screens/user_information_screen.dart
@@ -1,4 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:phone_auth_firebase/models/user_model.dart';
+import 'package:phone_auth_firebase/provider/auth_provider.dart';
+import 'package:phone_auth_firebase/screens/home_screen.dart';
+import 'package:phone_auth_firebase/utils/utils.dart';
+import 'package:phone_auth_firebase/widgets/custom_button.dart';
+import 'package:phone_auth_firebase/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class UserInformationScreen extends StatefulWidget {
   const UserInformationScreen({super.key});
@@ -8,8 +17,194 @@ class UserInformationScreen extends StatefulWidget {
 }
 
 class _UserInformationScreenState extends State<UserInformationScreen> {
+  File? image;
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController bioController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    nameController.dispose();
+    emailController.dispose();
+    bioController.dispose();
+  }
+
+  //* Handle selecting image
+  void selectImage() async {
+    image = await pickImage(context);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final isLoading =
+        Provider.of<AuthProvider>(context, listen: true).isLoading;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: isLoading == true
+            ? const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.purple,
+                ),
+              )
+            : SingleChildScrollView(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 25.0, horizontal: 5.0),
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      InkWell(
+                        onTap: () => selectImage(),
+                        child: image == null
+                            ? const CircleAvatar(
+                                backgroundColor: Colors.purple,
+                                radius: 50,
+                                child: Icon(
+                                  Icons.account_circle,
+                                  size: 50,
+                                  color: Colors.white,
+                                ),
+                              )
+                            : CircleAvatar(
+                                backgroundImage: FileImage(image!),
+                                radius: 50,
+                              ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 15),
+                        margin: const EdgeInsets.only(top: 20),
+                        child: Column(
+                          children: <Widget>[
+                            // name field
+                            textField(
+                              hintText: "John Smith",
+                              icon: Icons.account_circle,
+                              inputType: TextInputType.name,
+                              maxLines: 1,
+                              controller: nameController,
+                            ),
+
+                            // email field
+                            textField(
+                              hintText: "john@example.com",
+                              icon: Icons.email,
+                              inputType: TextInputType.emailAddress,
+                              maxLines: 1,
+                              controller: emailController,
+                            ),
+
+                            // bio field
+                            textField(
+                              hintText: "Enter your bio here...",
+                              icon: Icons.edit,
+                              inputType: TextInputType.multiline,
+                              maxLines: null,
+                              controller: bioController,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width * 0.90,
+                        child: CustomButton(
+                          text: "Continue",
+                          onPressed: () => storeUserData(),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+      ),
+    );
+  }
+
+  Widget textField({
+    required String hintText,
+    required IconData icon,
+    required TextInputType inputType,
+    int? maxLines,
+    required TextEditingController controller,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: TextFormField(
+        cursorColor: Colors.purple,
+        controller: controller,
+        keyboardType: inputType,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          prefixIcon: Container(
+            margin: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.purple,
+            ),
+            child: Icon(
+              icon,
+              size: 20,
+              color: Colors.white,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          hintText: hintText,
+          alignLabelWithHint: true,
+          border: InputBorder.none,
+          fillColor: Colors.purple.shade50,
+          filled: true,
+        ),
+      ),
+    );
+  }
+
+  // save user data to firestore database
+  void storeUserData() async {
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
+    UserModel userModel = UserModel(
+      name: nameController.text.trim(),
+      email: emailController.text.trim(),
+      bio: bioController.text.trim(),
+      profilePic: "",
+      createdAt: "",
+      phoneNumber: "",
+      uid: "",
+    );
+
+    if (image != null) {
+      authProvider.saveUserDataToFirebase(
+          context: context,
+          userModel: userModel,
+          profilePic: image!,
+          onSuccess: () {
+            // once data is uploaded, store it locally in sharedpreferences
+            authProvider.saveDataToSharedPreferences().then((value) =>
+              authProvider.setSignIn().then((value) =>
+                nextScreenPushAndRemoveUntil(context, const HomeScreen())
+              )
+            );
+          });
+    } else {
+      showSnackBar(context, "Please upload your profile  photo");
+    }
   }
 }
